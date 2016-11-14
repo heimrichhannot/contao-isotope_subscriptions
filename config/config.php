d00a06a48c1d0a240d3ab842fbe96009d0450d35
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-11-14 10:02:33 +0100

    fixed export connection

--- config/config.php
+++ config/config.php
@@ -3,41 +3,45 @@
 /**
  * Backend modules
  */
-HeimrichHannot\HastePlus\Arrays::insertInArrayByName($GLOBALS['BE_MOD']['isotope'], 'iso_rules', array(
-	'iso_subscriptions' => array(
-		'tables' 			=> array('tl_iso_subscription_archive', 'tl_iso_subscription'),
-		'icon' 				=> 'system/modules/isotope_subscriptions/assets/img/icon.png',
-		'export'			=> array('HeimrichHannot\IsotopeSubscriptions\IsotopeSubscriptions', 'export')
-	))
+HeimrichHannot\HastePlus\Arrays::insertInArrayByName(
+    $GLOBALS['BE_MOD']['isotope'],
+    'iso_rules',
+    array(
+        'iso_subscriptions' => array(
+            'tables'     => array('tl_iso_subscription_archive', 'tl_iso_subscription'),
+            'icon'       => 'system/modules/isotope_subscriptions/assets/img/icon.png',
+            'export_xls' => \HeimrichHannot\Exporter\ModuleExporter::getBackendModule(),
+        ),
+    )
 );
 
 /**
  * Frontend modules
  */
 $GLOBALS['FE_MOD']['isotope_subscriptions'] = array(
-	'iso_activation' => 'Isotope\Module\Activation',
-	'iso_cancellation' => 'Isotope\Module\Cancellation'
+    'iso_activation'   => 'Isotope\Module\Activation',
+    'iso_cancellation' => 'Isotope\Module\Cancellation',
 );
 
 /**
  * Hooks
  */
 $GLOBALS['ISO_HOOKS']['preCheckout']['setCheckoutModuleIdSubscriptions'] = array('Isotope\IsotopeSubscriptions', 'setCheckoutModuleIdSubscriptions');
-$GLOBALS['ISO_HOOKS']['preCheckout']['checkForExistingSubscription'] = array('Isotope\IsotopeSubscriptions', 'checkForExistingSubscription');
-$GLOBALS['ISO_HOOKS']['postCheckout']['addSubscriptions'] = array('Isotope\IsotopeSubscriptions', 'addSubscriptions');
+$GLOBALS['ISO_HOOKS']['preCheckout']['checkForExistingSubscription']     = array('Isotope\IsotopeSubscriptions', 'checkForExistingSubscription');
+$GLOBALS['ISO_HOOKS']['postCheckout']['addSubscriptions']                = array('Isotope\IsotopeSubscriptions', 'addSubscriptions');
 
 /**
  * Notification center notification types
  */
 $arrNotifications = &$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['isotope'];
 
-$arrNotifications['iso_subscription_activation'] = $arrNotifications['iso_order_status_change'];
+$arrNotifications['iso_subscription_activation']                 = $arrNotifications['iso_order_status_change'];
 $arrNotifications['iso_subscription_activation']['email_text'][] = 'link';
 
 /**
  * Models
  */
-$GLOBALS['TL_MODELS'][\Isotope\Model\Subscription::getTable()] = 'Isotope\Model\Subscription';
+$GLOBALS['TL_MODELS'][\Isotope\Model\Subscription::getTable()]        = 'Isotope\Model\Subscription';
 $GLOBALS['TL_MODELS'][\Isotope\Model\SubscriptionArchive::getTable()] = 'Isotope\Model\SubscriptionArchive';
 
 
